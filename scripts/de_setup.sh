2b61766902348003c366267aed105feceaac3fe0
Maciej Ziaja <maciejzjg@gmail.com>
2020-03-20 21:37:25 +0100

    Remove screensaver

--- scripts/de_setup.sh
+++ scripts/de_setup.sh
@@ -12,8 +12,8 @@ function config_xfce_theming
 	xfconf-query -c xfwm4 -p /general/theme -s 'Arc-Dark'
 	# Set icons
 	xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
-	# Disable screensaver
-	xfconf-query -c xfce4-session -nt bool -p /startup/screensaver/enabled -s false
+	# Remove screensaver
+	sudo apt remove -y xscreensaver
 	# Make qt5 use gtk theme
 	sudo sh -c "echo 'QT_QPA_PLATFORMTHEME=gtk2' >> /etc/environment"
 }
