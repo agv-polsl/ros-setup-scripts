2a8247c82a0171b0a9b673a64353127459e3ae76
Maciej Ziaja <maciejzjg@gmail.com>
2020-03-20 19:55:20 +0100

    Add pip requirements

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,9 +1,9 @@
 function set_colors
 {
-	GREEN='\033[1;32m'
-	RED='\033[1;31m'
-	BLUE='\033[1m\033[34m'
-	NOCOLOR='\033[0m'
+	export GREEN='\033[1;32m'
+	export RED='\033[1;31m'
+	export BLUE='\033[1m\033[34m'
+	export NOCOLOR='\033[0m'
 }
 
 function error
