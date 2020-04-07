82c50301c3ac81e00a0eadc2dc1c1f7acf00e1f5
asbird <ptasinskiwojciech@gmail.com>
2020-04-07 16:26:07 +0000

    melodic branch

--- scripts/ros_setup.sh
+++ scripts/ros_setup.sh
@@ -6,14 +6,19 @@ function install_rosdeps
 
 function install_ros
 {
-	ROS_KEY_URL=https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-	curl -s $ROS_KEY_URL | sudo apt-key add -
-	sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu \
-		`lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+	sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654z
+	curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 	
 	sudo apt update
 	sudo apt -y install --no-install-recommends `cat pkglist-ros`
+
+	echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+	source ~/.bashrc
+
+	sudo rosdep init
+	rosdep update
 }
 
 function install_gazebo
